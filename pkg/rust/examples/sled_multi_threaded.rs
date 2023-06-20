68825538344e76ed5b017cea6a469a15da3277f9
Junyoung Choi <cjy13753@gmail.com>
2023-06-20 21:20:51 +0900

    Add RwLock to concurrently insert values with sled storage

--- pkg/rust/examples/sled_multi_threaded.rs
+++ pkg/rust/examples/sled_multi_threaded.rs
@@ -7,6 +7,7 @@ mod sled_multi_threaded {
             sled_storage::SledStorage,
         },
         std::thread,
+        std::sync::{Arc, RwLock},
     };
 
     pub async fn run() {
@@ -23,28 +24,36 @@ mod sled_multi_threaded {
             SledStorage supports cloning, using this we can create copies of the storage for new threads;
             all we need to do is wrap it in glue again.
         */
-        let insert_storage = storage.clone();
-        let insert_thread = thread::spawn(move || {
-            let mut glue = Glue::new(insert_storage);
-            let query = "INSERT INTO greet (name) VALUES ('Foo')";
+        
+        let data = Arc::new(RwLock::new(0));
+        
+        let insert_foo_storage = storage.clone();
+        let data_clone = Arc::clone(&data);
+        let insert_foo_thread = thread::spawn(move || {
+            let mut _data_clone = data_clone.write().unwrap();
+            
+            let mut glue = Glue::new(insert_foo_storage);
+            let query = "INSERT INTO greet (name) VALUES ('Foo');";
 
             block_on(glue.execute(query)).unwrap();
         });
 
-        let select_storage = storage;
-        let select_thread = thread::spawn(move || {
-            let mut glue = Glue::new(select_storage);
-            let query = "SELECT * FROM greet;";
+        let insert_world_storage = storage.clone();
+        let data_clone = Arc::clone(&data);
+        let insert_world_thread = thread::spawn(move || {
+            let mut _data_clone = data_clone.write().unwrap();
+            
+            let mut glue = Glue::new(insert_world_storage);
+            let query = "INSERT INTO greet (name) VALUES ('World');";
 
-            let payloads = block_on(glue.execute(query)).unwrap();
-            println!("{payloads:?}");
+            block_on(glue.execute(query)).unwrap();
         });
 
-        select_thread
+        insert_world_thread
             .join()
             .expect("Something went wrong in the world thread");
 
-        insert_thread
+        insert_foo_thread
             .join()
             .expect("Something went wrong in the foo thread");
 
